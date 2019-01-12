8cc2a9b2dc5a496c8514a6948e3997210284cfda
AlbertoTorres <a.torres@2acad.com>
2019-01-12 17:10:55 +0100

    Inicial

---
+++ frmInicio.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmInicio
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCSV = New System.Windows.Forms.Button()
+        Me.txtCSV = New System.Windows.Forms.TextBox()
+        Me.dgv1 = New System.Windows.Forms.DataGridView()
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnCSV
+        '
+        Me.btnCSV.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCSV.Location = New System.Drawing.Point(685, 12)
+        Me.btnCSV.Name = "btnCSV"
+        Me.btnCSV.Size = New System.Drawing.Size(106, 32)
+        Me.btnCSV.TabIndex = 0
+        Me.btnCSV.Text = "Cargar CSV"
+        Me.btnCSV.UseVisualStyleBackColor = True
+        '
+        'txtCSV
+        '
+        Me.txtCSV.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtCSV.Location = New System.Drawing.Point(12, 16)
+        Me.txtCSV.Name = "txtCSV"
+        Me.txtCSV.Size = New System.Drawing.Size(664, 22)
+        Me.txtCSV.TabIndex = 1
+        '
+        'dgv1
+        '
+        Me.dgv1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv1.Location = New System.Drawing.Point(12, 57)
+        Me.dgv1.Name = "dgv1"
+        Me.dgv1.RowTemplate.Height = 24
+        Me.dgv1.Size = New System.Drawing.Size(776, 381)
+        Me.dgv1.TabIndex = 2
+        '
+        'frmInicio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dgv1)
+        Me.Controls.Add(Me.txtCSV)
+        Me.Controls.Add(Me.btnCSV)
+        Me.Name = "frmInicio"
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCSV As Forms.Button
+    Friend WithEvents txtCSV As Forms.TextBox
+    Friend WithEvents dgv1 As Forms.DataGridView
+End Class
---
+++ frmInicio.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Windows.Forms
+Imports System.Data
+
+Public Class frmInicio
+    Dim fiCSV As String = ""
+    Public dTabla As DataTable
+    '
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        fiCSV = IO.Path.Combine(_appFull(appParte.Directorio), "DICTIONARY.txt")
+        If IO.File.Exists(fiCSV) Then
+            txtCSV.Text = fiCSV
+            CargarDAtos()
+        Else
+            txtCSV.Text = ""
+        End If
+    End Sub
+    Private Sub btnCSV_Click(sender As Object, e As EventArgs) Handles btnCSV.Click
+        Dim oFd As New OpenFileDialog With {
+            .InitialDirectory = _appFull(appParte.Directorio),
+            .Filter = "Fichero CSV (*.csv)|*.csv|Fichero TXT (*.txt)|*.txt",
+            .FilterIndex = 1
+        }
+        If oFd.ShowDialog = DialogResult.OK Then
+            txtCSV.Text = oFd.FileName
+        End If
+        CargarDAtos()
+    End Sub
+
+    Public Sub CargarDAtos()
+        If IO.File.Exists(fiCSV) = False Then Exit Sub
+        dTabla = New DataTable("DATOSCSV")
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(fiCSV, System.Text.Encoding.UTF8)
+            MyReader.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
+            MyReader.Delimiters = New String() {vbTab, ";"}
+            MyReader.CommentTokens = New String() {""}
+            'MyReader.HasFieldsEnclosedInQuotes = True
+            Dim currentRow As String()
+            'Loop through all of the fields in the file. 
+            'If any lines are corrupt, report an error and continue parsing. 
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+                    If MyReader.LineNumber = 2 Then
+                        '' Crear las cabeceras
+                        For Each tCol As String In currentRow
+                            Call dTabla.Columns.Add(tCol, GetType(String))
+                        Next
+                    Else
+                        '' Insertar los datos
+                        Dim newRow = dTabla.Rows.Add()
+                        newRow.ItemArray = currentRow
+                    End If
+                Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                    MsgBox("Line " & ex.Message &
+                    " is invalid.  Skipping")
+                End Try
+            End While
+            dgv1.DataSource = dTabla
+        End Using
+    End Sub
+
+    Public Function _appFull(Optional parte As appParte = appParte.Full) As String
+        Select Case parte
+            Case appParte.Full
+                Return System.Reflection.Assembly.GetEntryAssembly.Location
+            Case appParte.Directorio
+                Return IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly.Location)
+            Case appParte.NameSin
+                Return IO.Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly.Location)
+            Case appParte.Directorio
+                Return IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly.Location)
+            Case appParte.Extension
+                Return IO.Path.GetExtension(System.Reflection.Assembly.GetEntryAssembly.Location)
+            Case Else
+                Return System.Reflection.Assembly.GetEntryAssembly.Location
+        End Select
+    End Function
+    Public Enum appParte
+        Full
+        NameSin
+        Name
+        Extension
+        Directorio
+    End Enum
+End Class
